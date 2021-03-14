28dd26008e892e0055b15690e5986b34324f1d3f
temkinman <temkinman@mail.ru>
2021-03-14 18:34:21 +0300

    not working completely

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 !== 0) return false;
+
+    let brackets = "[]{}()<>||";
+    let input = str.split("");
+    let stack = [];
+
+    for (let bracket of input) {
+        let bracketInd = brackets.indexOf(bracket);
+
+        if (bracketInd % 2 === 0) {
+            if (
+                bracketInd === 8 &&
+                input.length % 2 === 0 &&
+                input.pop() === bracket
+            ) {
+                return true;
+            }
+            stack.push(bracketInd + 1);
+        } else {
+            if (stack.pop() !== bracketInd) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
